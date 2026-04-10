9c7dbc04134236bbd1ccd570f699696e14647896
Mbrokans <s25brokmart@venta.lv>
2026-04-10 18:25:06 +0300

    EDIT:added more functions

--- src/Datastr/MyHeap.java
+++ src/Datastr/MyHeap.java
@@ -3,16 +3,23 @@ package Datastr;
 
 public class MyHeap <Ttype>{
 	private MyNode<Ttype> rootNode = null;
-	private MyNode<Ttype> lastNode= null;
+	private MyNode<Ttype> lastNode = null;
 	private int howManyElements = 0;
+	private int level = 0;
 	
 	public int getHowManyElements() {
 		return howManyElements;
 	}
 	
+	//get funkciajs netaisām blokiem, lai lietotajs tiem netiek klāt
+	//set funkcijas netaisām, jo neļaujam lieottajam mainīt ne blokus, ne arī elementa skaitu
+	
+	//bez argumenta konstruktors nāks no Object klases, tāpec paši netaisām
+	
 	public boolean isEmpty() {
 		return (howManyElements == 0);
 	}
+	
 	public boolean isFull() {
 		try {
 			new MyNode('a');
@@ -21,5 +28,72 @@ public class MyHeap <Ttype>{
 			return true;
 		}
 
+	}
+
+	
+	public void add(Ttype element) throws Exception{
+		if(isFull()) {
+			throw new Exception("Kaudze ir pilna un nevar pievienot jaunu elementu");
+		}
+		
+		if(element == null) {
+			throw new Exception("Ievades elements nevar būt null");
+		}
+		
+		//ja kaudze ir tukša, tad jaunais bloks būs vienīagis
+		if(isEmpty()) {
+			MyNode newNode = new MyNode(element);
+			rootNode = newNode;
+			lastNode = newNode;
+			howManyElements++;
+		}
+		//jāpievieno kārtejais elements
+		else
+		{
+			MyNode newNode = new MyNode(element);
+			//pievienot jauno bloku, lai nodrošinātu formas īpašību
+			//pievienot pie sakni
+			if(howManyElements == 1) {
+				newNode.setParentNode(rootNode);
+				rootNode.setLeftChildNode(newNode);
+				lastNode = newNode;
+				level++;
+			}
+			
+			//noskaidrojam, vai ir eksistējošam blokam labās puses māsa/brālis
+			if(lastNode.getParentNode().getRightChildNode() == null) {
+				lastNode.getParentNode().setRightChildNode(newNode);
+				newNode.setParentNode(lastNode.getParentNode());
+				lastNode = newNode;
+			}
+			
+			//2^0 - 0. līmenī ir 1 bērns
+			//2^1 - 1. līmenī ir 2 bērni
+			//2^2 - 2. līmenī ir 4 bērni
+			int sum = 0;
+			for(int i = 0; i < level; i++) {
+				sum += Math.pow(2, i);
+			}
+			//ja lastNode ir konkrētajā līmenī pēdējais bloks
+			if(howManyElements == sum) {//šo var optimizēt ar howManyElements == 1 scenāriju
+				MyNode currentNode = rootNode;
+				
+				while(currentNode.getLeftChildNode()!=null) {
+					currentNode = currentNode.getLeftChildNode();
+				}
+				
+				currentNode.setLeftChildNode(newNode);
+				newNode.setParentNode(currentNode);
+				lastNode = newNode;
+			}
+			
+			
+			
+			
+			//TODO reheapUp(newNode);
+			howManyElements++;
+			
+		}
+		
 	}
 }

980f8ff2c210e36880ab1de966f9b4789d3f4f7e
Mbrokans <s25brokmart@venta.lv>
2026-04-10 17:24:27 +0300

    EDIT: added a isempty and is full functions

--- src/Datastr/MyHeap.java
+++ src/Datastr/MyHeap.java
@@ -1,5 +1,25 @@
 package Datastr;
 
-public class MyHeap {
 
+public class MyHeap <Ttype>{
+	private MyNode<Ttype> rootNode = null;
+	private MyNode<Ttype> lastNode= null;
+	private int howManyElements = 0;
+	
+	public int getHowManyElements() {
+		return howManyElements;
+	}
+	
+	public boolean isEmpty() {
+		return (howManyElements == 0);
+	}
+	public boolean isFull() {
+		try {
+			new MyNode('a');
+			return false;
+		} catch (OutOfMemoryError error) {
+			return true;
+		}
+
+	}
 }
